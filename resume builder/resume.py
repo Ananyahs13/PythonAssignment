af21be7b63eeaba032836746ed913a334deb0bfe
Ananyahs13 <106958855+Ananyahs13@users.noreply.github.com>
2023-12-31 16:21:45 +0530

    Update resume.py

--- resume builder/resume.py
+++ resume builder/resume.py
@@ -1,28 +1,67 @@
-def generate_resume(name, summary, technical_skills, project_details):
-    resume = f"Name: {name}\n"
-    resume += f"Summary: {summary}\n"
-    resume += f"Technical Skills: {technical_skills}\n\n"
 
-    resume += "Project Details:\n"
-    for project in project_details:
-        resume += f"Project Name: {project['name']}\n"
-        resume += f"Description: {project['description']}\n"
-        resume += f"Duration: {project['duration']}\n\n"
+from django.db import models
 
-    return resume
+class Resume(models.Model):
+    name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
 
+from django import forms
+from .models import Resume
 
-if __name__ == "__main__":
-    # Sample data
-    name = "Ananya"
-    summary = "Developer"
-    technical_skills = "Python, JavaScript, SQL"
-    project1 = {"name": "Project A", "description": "Web application development", "duration": "6 months"}
-    project2 = {"name": "Project B", "description": "Database design", "duration": "4 months"}
-    project_details = [project1, project2]
+class ResumeForm(forms.ModelForm):
+    class Meta:
+        model = Resume
+        fields = '__all__'
 
-    # Generate the resume
-    resume_text = generate_resume(name, summary, technical_skills, project_details)
+from django.shortcuts import render
+from .forms import ResumeForm
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
 
-    # Print the resume
-    print(resume_text)
+def generate_resume(request):
+    if request.method == 'POST':
+        form = ResumeForm(request.POST)
+        if form.is_valid():
+           
+            name = form.cleaned_data['name']
+            designation = form.cleaned_data['designation']
+
+            
+            response = HttpResponse(content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename="{name}_resume.pdf"'
+
+            p = canvas.Canvas(response)
+           
+            p.drawString(100, 800, f"Resume for {name}")
+            p.drawString(100, 780, f"Designation: {designation}")
+
+          
+            p.showPage()
+            p.save()
+
+            return response
+    else:
+        form = ResumeForm()
+
+    return render(request, 'generate_resume.html', {'form': form})
+<!-- ResumeApp/templates/generate_resume.html -->
+<form method="post" action="{% url 'generate_resume' %}">
+    {% csrf_token %}
+    {{ form.as_p }}
+    <button type="submit">Generate Resume</button>
+</form>
+
+from django.urls import path
+from .views import generate_resume
+
+urlpatterns = [
+    path('generate_resume/', generate_resume, name='generate_resume'),
+]
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('ResumeApp.urls')),
+]
